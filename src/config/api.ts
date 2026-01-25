858fb5d6023910751aff202c8a1ad9b83bec7574
MahmoudZakaria <88841099+MahmoudZakarya@users.noreply.github.com>
2026-01-25 20:42:36 +0200

    Support Electron environment in API config

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,8 @@
+const isElectron =
+  typeof navigator !== "undefined" && /electron/i.test(navigator.userAgent);
+
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  (import.meta.env.DEV ? "http://localhost:3000" : "");
+  (isElectron || import.meta.env.DEV ? "http://localhost:3000" : "");
 
 export default API_URL;
